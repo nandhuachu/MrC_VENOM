4dfdf98147d49526bba3a0ee7f59617d2bf4a553
Venom <67759177+nandhuachu@users.noreply.github.com>
2022-11-29 14:08:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MyBotzz/MYDQBotz
+  git clone https://github.com/nandhuachu/aishu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
